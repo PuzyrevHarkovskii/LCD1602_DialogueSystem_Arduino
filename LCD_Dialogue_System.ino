d116165125630b1e6460703613928d053a193f88
Владислав <90913741+PuzyrevHarkovskii@users.noreply.github.com>
2024-01-15 23:16:09 +0300

    Update LCD_Dialogue_System.ino

--- LCD_Dialogue_System.ino
+++ LCD_Dialogue_System.ino
@@ -1,11 +1,13 @@
 #define _LCD_TYPE 1
 #define BTN_PIN_1 3
 #define BTN_PIN_2 4
+#define BTN_PIN_3 5
 #include <LCD_1602_RUS_ALL.h>
 #include "GyverButton.h"
 
 GButton butt1(BTN_PIN_1);
 GButton butt2(BTN_PIN_2);
+GButton butt3(BTN_PIN_3);
 
 LCD_1602_RUS lcd(0x27, 16, 2);
 
@@ -25,7 +27,7 @@ void displayMessage(const char* line1, const char* line2, int delayTime = 2000) 
 void setup() {
   Serial.begin(9600);
 
-  butt1.setDebounce(50);  // Adjust debounce time if needed
+  butt1.setDebounce(50);  
   butt2.setDebounce(50);
 }
 
@@ -33,14 +35,18 @@ void loop() {
   lcd.init();
   lcd.backlight();
 
+  //Реплики
+
   displayMessage("Реплика 1", "", 2000);
   displayMessage("", "Реплика 2", 1000);
 
   delay(1000);
 
+  //Начало вопроса
+
   displayMessage("Первый вопрос", "1. Да     2. Нет");
 
-  // Wait for button press
+
   while (!butt1.isSingle() && !butt2.isSingle()) {
     butt1.tick();
     butt2.tick();
@@ -54,17 +60,18 @@ void loop() {
     displayMessage("Второй вариант", "");
   }
 
-  // Сброс состояния кнопок перед следующим вопросом
   butt1.resetStates();
   butt2.resetStates();
+  //Конец вопроса
+
+  //Начало вопроса
 
   displayMessage("Второй вопрос", "1. Да     2. Нет");
 
-  // Wait for button press
   while (!butt1.isSingle() && !butt2.isSingle()) {
     butt1.tick();
     butt2.tick();
-    delay(50);  // Adjust the delay as needed
+    delay(50); 
     Serial.println("Button 1: " + String(butt1.isSingle()) + " Button 2: " + String(butt2.isSingle()));
   }
 
@@ -74,9 +81,11 @@ void loop() {
     displayMessage("Второй вариант-2", "");
   }
 
-  // Сброс состояния кнопок перед следующим вопросом
+
   butt1.resetStates();
   butt2.resetStates();
+
+  //Конец вопроса
 
   displayMessage("Конец диалога", "", 2000);
 }
